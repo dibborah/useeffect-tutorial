cdad359fe0e5bfbb63fd1c7b9077b70495c006f5
dibya.borah <dibyajyotiborah469@gmail.com>
2024-04-11 17:18:35 +0530

    displayed fetched data by creating a new User component. Shown LOADING STATE along with ERROR HANDLING

--- src/ExampleFetchData.jsx
+++ src/ExampleFetchData.jsx
@@ -1,32 +1,38 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+import User from "./User";
 
 const ExampleFetchData = () => {
-    const [data, setFetchData] = useState([]);
-  const URL = "https://jsonplaceholder.typicode.com/posts";
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [errorMsg, setErrorMsg] = useState("");
+
+  const URL = "https://jsonplaceholder.typicode.com/users";
   // fetching using 2 approaches:
   // 1. promises
   // 2. using async await
 
   // using normal functions
-//   async function fetchData() {
-    // using promises
+  //   async function fetchData() {
+  // using promises
 
-    // fetch(URL)
-    //   .then((response) => {
-    //      return response.json();// you have to return the response in a json format than only after one more .than you'll can catch the data
-    //   })
-    //   .then((data) => {
-    //     console.log(data);
-    //   });
+  // fetch(URL)
+  //   .then((response) => {
+  //      return response.json();// you have to return the response in a json format than only after one more .than you'll can catch the data
+  //   })
+  //   .then((data) => {
+  //     console.log(data);
+  //   });
 
-    // using async await
-    // const response = await fetch(URL);
-    // const data = await response.json();
-    // console.log(data);
-//   }
+  // using async await
+  // const response = await fetch(URL);
+  // const data = await response.json();
+  // console.log(data);
+  //   }
 
-// using arraw function
+  // using arraw function
   const fetchData = async () => {
+    setIsLoading(true);
     // using promises
 
     // fetch(URL)
@@ -39,18 +45,47 @@ const ExampleFetchData = () => {
 
     // using async await
     const response = await fetch(URL);
+    console.log(response);
+    if(!(response.status >= 200 && response.status < 300)){
+      setIsError(true);
+      setErrorMsg(`${response.status} Error`)
+      setIsLoading(false);
+      return;// Write return statement if you do not want to excute the further code in the function if the if condition is true
+    }
     const data = await response.json();
-    console.log(data);
-    setFetchData(data);
-  }
+    console.log("Executed response.json()");
+    setUsers(data);
+    console.log("Executed setUser()");
+    // setTimeout(() => {
+      setIsLoading(false);
+    // }, 2000);
+    console.log("Executed setTimeout");
+  };
   useEffect(() => {
     fetchData();
-  }, []);
+  }, []); // empty dependency => useEffect ek bar hi call hoga initial render ke baad
+
+  if (isLoading) {
+    return (
+      <h1 style={{ textAlign: "center", margin: "10rem 0" }}>...Loading</h1>
+    );
+  }
+
+  if(isError){
+    return <h1 style={{ textAlign: "center", margin: "10rem 0", color: "red" }}>{errorMsg}</h1>
+  }
+
   return (
-    <div>
-      <h1>ExampleFetchData</h1>
-    </div>
+    <Fragment>
+      {users.map((user) => (
+        <User {...user} key={user.id} />
+      ))}
+    </Fragment>
   );
 };
 
 export default ExampleFetchData;
+
+// Learn Todo:
+
+// 1. learn the different scenerios when useEffect goes in an infinite loop
---
+++ src/User.jsx
@@ -0,0 +1,22 @@
+    function User({address, company, email, id, name, phone, username, website }) {
+  return (
+    <div style={{
+        padding: "1rem",
+        margin:"1rem",
+        backgroundColor:"#000",
+        color:"#efefef",
+        textAlign:"center"
+    }}>
+      <h4>id: {id}</h4>
+      <h4>name: {name}</h4>
+      <h4>phone: {phone}</h4>
+      <h4>company: {company.name}</h4>
+      <h4>email: {email}</h4>
+      <h4>username: {username}</h4>
+      <h4>website: {website}</h4>
+      <h4>address: {address.city}</h4>
+    </div>
+  );
+}
+
+export default User;
